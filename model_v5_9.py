b810ab9214bdfe1f3431e571bc60dce44f005a5b
nir36900 <nirlimbu@gmail.com>
2025-07-06 00:43:17 +0545

    Add files via upload

---
+++ model_v5_9.py
@@ -0,0 +1,6 @@
+
+def model_v5_9_logic(df):
+    cond1 = df['Volume'] > df['Volume'].rolling(3).mean()
+    cond2 = df['Price_Change_Pct'] > 0
+    cond3 = df['Rolling_Avg_5'] < df['Close']
+    return cond1 & cond2 & cond3
